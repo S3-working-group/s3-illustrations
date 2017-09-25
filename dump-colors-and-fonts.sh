4dce4aafc3c37c548b653bd29714a253e69111e1
bboc <bernhard.bockelbrink@gmail.com>
2017-09-25 22:22:57 +0200

    updates script, new dump of colors

--- dump-colors-and-fonts.sh
+++ dump-colors-and-fonts.sh
@@ -1,3 +1,6 @@
-ls -b src | xargs -I {} ogtool dump "src/{}" -v
-ogtool run-plugin combine_colors_and_fonts dummy.graffle collect.yaml
-rm  dummy-combine_colors_and_fonts.graffle
+mkdir tmp
+cd tmp
+ls -b ../src | xargs -I {} ogtool dump "../src/{}"
+ogtool run-plugin combine_colors_and_fonts ../dummy.graffle --config ../collect.yaml
+mv combined.* ..
+rm  ../dummy-combine_colors_and_fonts.graffle
