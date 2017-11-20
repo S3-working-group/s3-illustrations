0bdd455653b62f112ca63de372b95e926ec2269b
bboc <bernhard.bockelbrink@gmail.com>
2017-11-20 15:48:57 +0100

    updated english pngs

--- export-en.sh
+++ export-en.sh
@@ -2,9 +2,9 @@
 echo "workon omnigraffle_export"
 
 # export png
-ls -b src | xargs -I {} ogexport png "graffle/src/{}" png/en/140dpi @140dpi.ini
+ls -b graffle/src | xargs -I {} ogexport png "graffle/src/{}" png/en/140dpi @140dpi.ini
 
 # export png with transparent background
-ls -b src | xargs -I {} ogexport png "src/{}" png/en/140dpi-transp @140dpi-transp.ini
+ls -b graffle/src | xargs -I {} ogexport png "graffle/src/{}" png/en/140dpi-transp @140dpi-transp.ini
 
 
