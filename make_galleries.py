6ddc76689edcf400c6f7a6971d313ddcc458561c
bboc <bernhard.bockelbrink@gmail.com>
2021-08-19 14:42:38 +0200

    fixed script

--- make_galleries.py
+++ make_galleries.py
@@ -28,11 +28,11 @@ title: The Sociocracy 3.0 Illustration Repository
 def make_galleries():
 
     for language in os.listdir('png'):
-        with (file('docs/gallery/index-%s.md' % language, 'w')) as index:
+        with (open('docs/gallery/index-%s.md' % language, 'w')) as index:
             index.write(INDEX_TEMPLATE % dict(lang=language))
             for d in sorted(os.listdir(os.path.join('png', language))):
                 index.write("- [%s](/gallery/index-%s-%s.html)\n" % (d, language, d))
-                with (file("docs/gallery/index-%s-%s.md" % (language, d), 'w+')) as gallery:
+                with (open("docs/gallery/index-%s-%s.md" % (language, d), 'w+')) as gallery:
                     gallery.write(GALLERY_TEMPLATE % dict(dir=d, lang=language))
                     for i in sorted(os.listdir(os.path.join('png', language, d))):
                         gallery.write("## %s\n\n" % i)
